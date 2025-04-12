83cfa4486dbfc87be4d4a6299a61d8b0c182be9b
DED <ded@ded>
2025-04-13 01:17:23 +0300

    Создание модели Users со связью 'один ко многим' с моделью Hotel. Выполнение миграций.(python manage.py makemigrations и python manage.py migrate). Подключение модели(таблицы) к админке.

--- control/admin.py
+++ control/admin.py
@@ -1,12 +1,23 @@
 from django.contrib import admin
 
-from .models import Hotel
+from .models import Hotel, Users
 
 
 # Подключение модели для отображения и управления в админпанели.
 class HotelAdmin(admin.ModelAdmin):
     list_display = ('name', 'price')  # Поля отображения в админке.
+    search_fields = ('name',)  # Поле поиска.
     ordering = ('name',)  # Сортировка.
 
 
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'name', 'phone_number', 'email', 'hotel', 'booking_number'
+    )  # Поля для отображения в админке.
+    search_fields = ('name', 'phone_number', 'email')  # Поля для поиска.
+    ordering = ('name',)  # Сортировка.
+
+
+# Регистрация моделей в админпанели.
 admin.site.register(Hotel, HotelAdmin)
+admin.site.register(Users, UserAdmin)
--- control/models.py
+++ control/models.py
@@ -9,12 +9,37 @@ class Hotel(models.Model):
     description = models.TextField(blank=True, null=True,
                                    verbose_name='Описание')
 
-    def __str__(self):
-        return self.name
-
+    # Управление поведением модели и взаимодействием с бв.
     class Meta:
         managed = False  # Игнорирование создание существующей таблицы.
         db_table = 'hotel'  # Имя таблицы, которая уже есть в в бд.
 
         verbose_name = 'Гостиница'
         verbose_name_plural = 'Гостиницы'
+
+    # Отображение в виде строки.
+    def __str__(self):
+        return self.name
+
+
+class Users(models.Model):
+    name = models.CharField(max_length=100, blank=False, null=False,
+                            verbose_name='Имя пользователя')
+    phone_number = models.CharField(max_length=20, blank=False,
+                                    null=False, verbose_name='№ телефона')
+    email = models.EmailField(max_length=100, blank=True,
+                              null=True, verbose_name='Электронная почта')
+    hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE,
+                              verbose_name='Гостиница')  # Связь с Hotel.
+    booking_number = models.TextField(blank=False, null=False,
+                                      verbose_name='Номер бронирования')
+
+    class Meta:
+        managed = False
+        db_table = 'users'
+
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.name
